cd5c661e7b81a5200d4ba86a21c4b16384a954d4
Jonas <jonaselemented@gmail.com>
2024-07-12 11:52:19 -0700

    Login and Register updated

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,33 +1,67 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import GlobalApi from "@/lib/GlobalApi";
+import { toast } from "sonner";
 
 function LoginPage() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const router = useRouter();
+
+  const onSignIn = (event) => {
+    event.preventDefault();
+    GlobalApi.signUpUser(email, password).then(
+      (resp) => {
+        console.log(resp.data.user);
+        console.log(resp.data.jwt);
+        sessionStorage.setItem("user", JSON.stringify(resp.data.user));
+        sessionStorage.setItem("jwt", resp.data.jwt);
+        toast("You logged in successfully");
+        router.push("/");
+      },
+      (e) => {
+        console.log(e);
+        toast("A problem has occurred in the server");
+      }
+    );
+  };
+
+  useEffect(() => {
+    const jwt = sessionStorage.getItem("jwt");
+
+    if (jwt) {
+      router.push("/");
+    }
+  }, []);
   return (
     <div className="p-20 flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-        <form>
+        <form onSubmit={onSignIn}>
           <div className="mb-4">
-            <label className="block text-gray-700 mb-2" htmlFor="username">
-              Username
+            <label className="block text-gray-700 mb-2" htmlFor="Email">
+              Email
             </label>
-            <Input
+            <input
               type="text"
-              id="username"
+              id="email"
               className="w-full px-3 py-2 border rounded-lg"
-              placeholder="Enter your username"
+              placeholder="Enter your email"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-4">
             <label className="block text-gray-700 mb-2" htmlFor="password">
               Password
             </label>
-            <Input
+            <input
               type="password"
               id="password"
               className="w-full px-3 py-2 border rounded-lg"
               placeholder="Enter your password"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <button
--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -1,43 +1,85 @@
-import { Input } from "@/components/ui/input";
-import React from "react";
+"use client";
+
+import GlobalApi from "@/lib/GlobalApi";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const onCreateAccount = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    GlobalApi.registerUser(username, email, password).then(
+      (resp) => {
+        console.log(resp.data.user);
+        console.log(resp.data.jwt);
+        sessionStorage.setItem("user", JSON.stringify(resp.data.user));
+        sessionStorage.setItem("jwt", resp.data.jwt);
+        setUsername("");
+        setEmail("");
+        setPassword("");
+        toast("Account created successfully!");
+        router.push("/");
+      },
+      (e) => {
+        toast("Error while creating account");
+      }
+    );
+  };
+
+  useEffect(() => {
+    const jwt = sessionStorage.getItem("jwt");
+
+    if (jwt) {
+      router.push("/");
+    }
+  }, []);
+
   return (
     <div className="p-20 flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
         <h2 className="text-2xl font-bold mb-6 text-center">Register</h2>
-        <form>
+        <form onSubmit={onCreateAccount}>
           <div className="mb-4">
             <label className="block text-gray-700 mb-2" htmlFor="username">
               Username
             </label>
-            <Input
+            <input
               type="text"
               id="username"
               className="w-full px-3 py-2 border rounded-lg"
               placeholder="Enter your username"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
             />
           </div>
           <div className="mb-4">
             <label className="block text-gray-700 mb-2" htmlFor="email">
               Email
             </label>
-            <Input
+            <input
               type="email"
               id="email"
               className="w-full px-3 py-2 border rounded-lg"
               placeholder="Enter your email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
           </div>
           <div className="mb-4">
             <label className="block text-gray-700 mb-2" htmlFor="password">
               Password
             </label>
-            <Input
+            <input
               type="password"
               id="password"
               className="w-full px-3 py-2 border rounded-lg"
               placeholder="Enter your password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
             />
           </div>
           <button
