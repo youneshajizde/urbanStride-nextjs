4dc2a2c0714bc2895e53c24d6cf0334c0d18bebc
Jonas <jonaselemented@gmail.com>
2024-08-07 13:12:35 -0700

    overload

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -143,71 +143,80 @@ function Navbar() {
           )}
         </div>
         <div className="hidden sm:flex items-center gap-6">
-          <Popover onOpenChange={handleNotificationOpen}>
-            <PopoverTrigger asChild className="relative">
-              <Button
-                variant="outline"
-                className="flex items-center gap-1 rounded-full"
-              >
-                <Bell className="font-semibold w-4 text-gray-700" />
-                {showBadge && (
-                  <span className="absolute top-0 right-0 inline-block w-3 h-3 bg-red-500 text-white text-xs font-bold rounded-full text-center leading-none">
-                    1
-                  </span>
-                )}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-full p-0">
-              <div className="flex items-center justify-between text-gray-500 p-2">
-                <h4 className="text-md">Notification</h4> <CircleX />
-              </div>
-              <hr />
-              <div className="p-2">
-                <h3 className="text-gray-500 font-medium text-xs">TODAY</h3>
-                <div className="space-y-1 mt-2">
-                  <h4 className="text-sm font-semibold flex items-center gap-1">
-                    UrbanStride <Image src={verify} width={17} height={17} />
-                  </h4>
-                  <p className="text-gray-500 text-xs font-medium flex items-center gap-1">
-                    You have logged in successfully Jonas Galliard! Don't miss
-                    our special offers
-                  </p>
-                  <div className="flex gap-2 border border-gray-200 w-full h-[80px] rounded-lg p-2">
-                    <div className="w-[30%] h-full bg-gray-300 rounded-md relative">
-                      <Image
-                        src={notifPhoto}
-                        width={0}
-                        height={0}
-                        sizes="100vw"
-                        objectFit="cover"
-                        fill
-                        className="rounded-md"
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <h2 className="text-xs font-semibold bg-gradient-to-r from-orange-600 to-orange-800 bg-clip-text text-transparent">
-                        Special offer
-                      </h2>
-                      <h5 className="text-sm font-semibold mt-1">
-                        New Balance Br
-                      </h5>
-                      <span className="flex gap-1">
-                        <Star fill="yellow" strokeWidth={0} className="w-4" />
-                        <span className="flex items-center gap-2">
-                          <span className="text-xs font-medium">5</span>
-                          <span className="text-gray-300">•</span>
-                          <span className="text-gray-500 font-light text-xs">
-                            8 items sold
-                          </span>
-                        </span>
+          {isLogged && (
+            <>
+              <Popover onOpenChange={handleNotificationOpen}>
+                <PopoverTrigger asChild className="relative">
+                  <Button
+                    variant="outline"
+                    className="flex items-center gap-1 rounded-full"
+                  >
+                    <Bell className="font-semibold w-4 text-gray-700" />
+                    {showBadge && (
+                      <span className="absolute top-0 right-0 inline-block w-3 h-3 bg-red-500 text-white text-xs font-bold rounded-full text-center leading-none">
+                        1
                       </span>
+                    )}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-full p-0">
+                  <div className="flex items-center justify-between text-gray-500 p-2">
+                    <h4 className="text-md">Notification</h4> <CircleX />
+                  </div>
+                  <hr />
+                  <div className="p-2">
+                    <h3 className="text-gray-500 font-medium text-xs">TODAY</h3>
+                    <div className="space-y-1 mt-2">
+                      <h4 className="text-sm font-semibold flex items-center gap-1">
+                        UrbanStride{" "}
+                        <Image src={verify} width={17} height={17} />
+                      </h4>
+                      <p className="text-gray-500 text-xs font-medium flex items-center gap-1">
+                        You have logged in successfully Jonas Galliard! Don't
+                        miss our special offers
+                      </p>
+                      <div className="flex gap-2 border border-gray-200 w-full h-[80px] rounded-lg p-2">
+                        <div className="w-[30%] h-full bg-gray-300 rounded-md relative">
+                          <Image
+                            src={notifPhoto}
+                            width={0}
+                            height={0}
+                            sizes="100vw"
+                            objectFit="cover"
+                            fill
+                            className="rounded-md"
+                          />
+                        </div>
+                        <div className="flex flex-col">
+                          <h2 className="text-xs font-semibold bg-gradient-to-r from-orange-600 to-orange-800 bg-clip-text text-transparent">
+                            Special offer
+                          </h2>
+                          <h5 className="text-sm font-semibold mt-1">
+                            New Balance Br
+                          </h5>
+                          <span className="flex gap-1">
+                            <Star
+                              fill="yellow"
+                              strokeWidth={0}
+                              className="w-4"
+                            />
+                            <span className="flex items-center gap-2">
+                              <span className="text-xs font-medium">5</span>
+                              <span className="text-gray-300">•</span>
+                              <span className="text-gray-500 font-light text-xs">
+                                8 items sold
+                              </span>
+                            </span>
+                          </span>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            </PopoverContent>
-          </Popover>
-          <Cart />
+                </PopoverContent>
+              </Popover>
+              <Cart />
+            </>
+          )}
 
           <Link href={"/login"}>
             <span className="flex items-center gap-2">
@@ -234,124 +243,46 @@ function Navbar() {
         }`}
       >
         <div className="flex flex-col items-start gap-4 p-4">
-          <Popover>
-            <PopoverTrigger asChild className="bg-gray-200 rounded-full w-full">
-              <Button
-                variant="outline"
-                className="flex items-center gap-1 rounded-full w-full"
-              >
-                <Bell className="font-semibold w-4 text-gray-700" />
-                <span className="ml-1 text-xs">Notifications</span>
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-80 p-0">
-              <div className="flex items-center justify-between text-gray-500 p-2">
-                <h4 className="text-md">Notification</h4> <CircleX />
-              </div>
-              <hr />
-              <div className="p-2">
-                <h3 className="text-gray-500 font-medium text-xs">TODAY</h3>
-                <div className="space-y-1 mt-2">
-                  <h4 className="text-sm font-semibold flex items-center gap-1">
-                    UrbanStride <Image src={verify} width={17} height={17} />
-                  </h4>
-                  <p className="text-gray-500 text-xs font-medium flex items-center gap-1">
-                    You have logged in successfully Jonas Galliard! Don't miss
-                    our special offers
-                  </p>
-                  <div className="flex gap-2 border border-gray-200 w-full h-[80px] rounded-lg p-2">
-                    <div className="w-[30%] h-full bg-gray-300 rounded-md relative">
-                      <Image
-                        src={notifPhoto}
-                        width={0}
-                        height={0}
-                        sizes="100vw"
-                        objectFit="cover"
-                        fill
-                        className="rounded-md"
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <h2 className="text-xs font-semibold bg-gradient-to-r from-orange-600 to-orange-800 bg-clip-text text-transparent">
-                        Special offer
-                      </h2>
-                      <h5 className="text-sm font-semibold mt-1">
-                        New Balance Br
-                      </h5>
-                      <span className="flex gap-1">
-                        <Star fill="yellow" strokeWidth={0} className="w-4" />
-                        <span className="flex items-center gap-2">
-                          <span className="text-xs font-medium">5</span>
-                          <span className="text-gray-300">•</span>
-                          <span className="text-gray-500 font-light text-xs">
-                            8 items sold
-                          </span>
-                        </span>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </PopoverContent>
-          </Popover>
-          <Popover>
-            <PopoverTrigger asChild className="bg-gray-200 rounded-full w-full">
-              <Button
-                variant="outline"
-                className="flex items-center gap-1 rounded-full w-full"
-              >
-                <h3 className="flex items-center gap-2 rounded-full md:py-2 md:px-2 py-2 px-3 text-xs font-medium">
-                  <ShoppingBag className="w-4" />
-                  <span className="text-xs hidden lg:block">2 items</span>
-                </h3>
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-80 p-0">
-              <div className="flex items-center justify-between text-gray-500 p-2">
-                <h4 className="text-md">Your Basket</h4> <CircleX />
-              </div>
-              <hr />
-              <div className="p-2">
-                <div className="flex justify-between">
-                  <div className="flex gap-2 border border-gray-200 w-full h-[100px] rounded-lg p-2">
-                    <div className="w-[30%] h-full bg-gray-300 rounded-md relative">
-                      <Image
-                        src={notifPhoto}
-                        width={0}
-                        height={0}
-                        sizes="100vw"
-                        objectFit="cover"
-                        fill
-                        className="rounded-md"
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <h5 className="text-sm font-semibold mt-1">
-                        New Balance Br
-                      </h5>
-                      <span className="flex gap-1">
-                        <Star fill="yellow" strokeWidth={0} className="w-4" />
-                        <span className="flex items-center gap-2">
-                          <span className="text-xs font-medium">5</span>
-                          <span className="text-gray-300">•</span>
-                          <span className="text-gray-500 font-light text-xs">
-                            8 items sold
-                          </span>
-                        </span>
-                      </span>
-                      <h3 className="text-xs text-gray-500 font-semibold">
-                        Size: Medium
-                      </h3>
-                      <span className="flex justify-between items-center w-full">
-                        <h3 className="text-xs font-semibold mt-1">$500</h3>
-                        <SquareX className="w-5" />
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </PopoverContent>
-          </Popover>
+          {isLogged && (
+            <>
+              <Popover>
+                <PopoverTrigger
+                  asChild
+                  className="bg-gray-200 rounded-full w-full"
+                >
+                  <Button
+                    variant="outline"
+                    className="flex items-center gap-1 rounded-full w-full"
+                  >
+                    <Bell className="font-semibold w-4 text-gray-700" />
+                    <span className="ml-1 text-xs">Notifications</span>
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-80 p-0">
+                  <Notification />
+                </PopoverContent>
+              </Popover>
+              <Popover>
+                <PopoverTrigger
+                  asChild
+                  className="bg-gray-200 rounded-full w-full"
+                >
+                  <Button
+                    variant="outline"
+                    className="flex items-center gap-1 rounded-full w-full"
+                  >
+                    <h3 className="flex items-center gap-2 rounded-full md:py-2 md:px-2 py-2 px-3 text-xs font-medium">
+                      <ShoppingBag className="w-4" />
+                      <span className="text-xs hidden lg:block">2 items</span>
+                    </h3>
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-80 p-0">
+                  <Cart />
+                </PopoverContent>
+              </Popover>
+            </>
+          )}
           <Link href={"/profile"}>
             <span className="flex items-center gap-2">
               <Image
